bb79d91a260fca024572b28df692c415e2e03e7e
Pasindu Batagoda <pasindubatagoda@gmail.com>
2021-10-03 20:11:38 +0530

    FlatList implemented to display ChatRoomItems

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -3,28 +3,38 @@ import { Text, Image, View, StyleSheet } from "react-native";
 import styles from "./Styles";
 
 //Single chat view component
-export default function ChatRoomItem() {
+export default function ChatRoomItem({ chatRoom }) {
+  //const chatRoom = props.chatRoom; (ES6 destructuring)
+  //const {chatRoom} = props; (destructure further)
+
   //<Image> represent chat list profile image
   //view for message counter badge
+  //everything within return is JSX
+  const user = chatRoom.users[1];
   return (
     <View style={styles.container}>
       <Image
         source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+          uri: user.imageUri,
         }}
         style={styles.image}
       />
-      <View style={styles.badgeContainer}>
-        <Text style={styles.badgeText}>4</Text>
-      </View>
+
+      {/* Message badge only dispayed if there are messages.
+      AND condition used */}
+      {chatRoom.newMessages && (
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
+        </View>
+      )}
+
       <View style={styles.rightContainer}>
         <View style={styles.row}>
-          <Text style={styles.name}>Pasindu Batagoda</Text>
-          <Text style={styles.text}>11:11 AM</Text>
+          <Text style={styles.name}>{user.name}</Text>
+          <Text style={styles.text}>{chatRoom.lastMessage.createdAt}</Text>
         </View>
         <Text numberOfLines={1} style={styles.text}>
-          {" "}
-          Hey lets hangout?
+          {chatRoom.lastMessage.content}
         </Text>
       </View>
     </View>
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
 
-import { Text, Image, View, StyleSheet } from "react-native";
+import { Text, Image, View, StyleSheet, FlatList } from "react-native";
 import ChatRoomItem from "../components/ChatRoomItem";
 
+import chatRoomsData from "../assets/dummy-data/ChatRooms";
+
 export default function TabOneScreen() {
+  //FlatList implemented to display chatRoomItem components for each set of data in Array
+  //FlatList smartly displays data in a scrollable list
+  //data,renderItem are props passed for FlatList
+
   return (
     <View style={styles.page}>
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
-      <ChatRoomItem />
+      <FlatList
+        data={chatRoomsData}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
+      />
     </View>
   );
 }
