b131b68c1883e0f79f43abca794da789394f7426
Pasindu Batagoda <pasindubatagoda@gmail.com>
2021-10-29 15:58:54 +0530

    Text input UI completed

---
+++ components/MessageInput/MessageInput.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
+import {
+  SimpleLineIcons,
+  Feather,
+  MaterialCommunityIcons,
+  AntDesign,
+  Ionicons,
+} from "@expo/vector-icons";
+
+//useState to manage data within component. With UseState Hook
+//useState return an array with 2 values
+const MessageInput = () => {
+  const [message, setMessage] = useState("");
+
+  const sendMessage = () => {
+    //send message
+    console.warn("Sending: ", message);
+
+    //After sending message, reset input to empty string
+    setMessage("");
+  };
+
+  const onPlusClicked = () => {
+    console.warn("On plus clicked");
+  };
+
+  const onPress = () => {
+    if (message) {
+      sendMessage();
+    } else {
+      onPlusClicked();
+    }
+  };
+  return (
+    <View style={styles.root}>
+      <View style={styles.inputContainer}>
+        <SimpleLineIcons
+          name="emotsmile"
+          size={24}
+          color="#595959"
+          style={styles.icon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Textagram message..."
+          //binding state to text input by user
+          value={message}
+          //onChangeText requires calling setMessage function to update state with new text
+          onChangeText={setMessage}
+        />
+        <Feather name="camera" size={24} color="#595959" style={styles.icon} />
+        <MaterialCommunityIcons
+          name="microphone-outline"
+          size={24}
+          color="#595959"
+          style={styles.icon}
+        />
+      </View>
+      <Pressable onPress={onPress} style={styles.buttonContainer}>
+        {message ? (
+          <Ionicons name="send" size={18} color="white" />
+        ) : (
+          <AntDesign name="plus" size={24} color="white" />
+        )}
+      </Pressable>
+    </View>
+    //Plus icon change to send icon when user start typing
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  inputContainer: {
+    backgroundColor: "#f2f2f2",
+    flex: 1,
+    marginRight: 10,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: "#dedede",
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 5,
+  },
+  input: {
+    flex: 1,
+    marginHorizontal: 5,
+  },
+  icon: {
+    marginHorizontal: 5,
+  },
+  buttonContainer: {
+    width: 35,
+    height: 35,
+    backgroundColor: "#3777f0",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 35,
+  },
+});
+
+export default MessageInput;
